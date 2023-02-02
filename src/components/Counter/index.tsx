3da1e4a7683452b3f84b7dd35cabf7898cec6b2e
lademir <lademirjunior@hotmail.com>
2023-02-02 16:24:16 -0300

    Icons

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Container, Title } from "./styles";
+import { Container, CounterButton, CounterValue, MinusIcon, PlusIcon, Title } from "./styles";
 
 interface CounterProps {
     title: string;
@@ -9,10 +9,19 @@ interface CounterProps {
 
 export function Counter({ title, initialValue, goal }: CounterProps) {
     const [value, setValue] = useState(initialValue);
+
+    const increment = () => setValue(value + 1);
+    const decrement = () => setValue(value - 1);
     return (
         <Container>
             <Title>{title}</Title>
-
+            <CounterButton>
+                <MinusIcon />
+            </CounterButton>
+            <CounterValue>{value}</CounterValue>
+            <CounterButton>
+                <PlusIcon />
+            </CounterButton>
         </Container>
     );
 }
--- src/components/Counter/styles.ts
+++ src/components/Counter/styles.ts
@@ -1,3 +1,4 @@
+import { AntDesign } from "@expo/vector-icons";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
@@ -20,4 +21,26 @@ export const Title = styled.Text`
 	font-size: 18px;
 	font-weight: bold;
 	color: ${({ theme }) => theme.COLORS.WHITE};
+`;
+
+export const CounterButton = styled.TouchableOpacity``;
+
+export const PlusIcon = styled(AntDesign).attrs(({ theme }) => {
+	return {
+		name: "plus",
+		size: 24,
+		color: theme.COLORS.WHITE,
+	};
+})``;
+export const MinusIcon = styled(AntDesign).attrs(({ theme }) => {
+	return {
+		name: "minus",
+		size: 24,
+		color: theme.COLORS.WHITE,
+	};
+})``;
+
+export const CounterValue = styled.Text`
+	color: ${({ theme }) => theme.COLORS.WHITE};
+	font-size: 24px;
 `;
